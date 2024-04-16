750decde9bf817fd043829c6ec164d9e5ba6be98
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-04-16 02:32:09 -0700

    New player function and tilemap for testing

--- game.py
+++ game.py
@@ -4,32 +4,35 @@ from pygame.locals import *
 from pytmx import load_pygame
 
 from scripts.settings import Settings
-from scripts.player import Player
+from scripts.entities import Player
+from scripts.tilemap import Tilemap
+
 
 # Use later
 vec = pg.math.Vector2  # 2 dimensional
 
-
 class Game:
     def __init__(self):
+        # Start pygame and default settings
         pg.init()
         self.settings = Settings()
         self.clock = pg.time.Clock()
-        self.screen = pg.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height), 0, 32
-        )
+        self.screen = pg.display.set_mode((self.settings.screen_width, self.settings.screen_height), 0, 32)
         pg.display.set_caption("The Forgotten Pages")
 
         # load map
         self.tmx_data = load_pygame("./assets/Map/map_final.tmx")
         self.world_offset = self.settings.world_offset
+        self.map_bg = pg.transform.scale(pg.image.load("./assets/Map/main_background.png").convert_alpha(), (self.settings.screen_width, self.settings.screen_height),)
 
         # Create first player
         # initial height is set to be (screen_height - 19) to avoid upward movement at the start of the game
 
         # Pass the game instance to the Player class
         # self.player = Player(self, 0, self.settings.screen_height - 19, 1)
-        self.player = Player(self, 0, self.screen.get_size()[1] - 19, 1)
+        # OLD self.player = Player(self, 0, self.screen.get_size()[1] - 19, 1.25)
+        self.player = Player(self, (0, self.screen.get_size()[1] - 19), (8, 16))
+        self.tilemap = Tilemap(self, tile_size=16)
 
         # Use a dictionary to store different types of sprites
         self.entities = {
@@ -41,6 +44,9 @@ class Game:
         # Player Movement Bools
         self.moving_left = False
         self.moving_right = False
+        self.movement = [False, False]
+
+        self.offset = [0, 0]
 
     def check_events(self):
         for event in pg.event.get():
@@ -52,25 +58,32 @@ class Game:
             if event.type == KEYDOWN:
                 key = event.key
                 if key == K_d or key == K_RIGHT:
-                    self.moving_right = True
+                    self.moving_right = True #OLD
+                    self.movement[1] = True # New
                 if key == K_a or key == K_LEFT:
-                    self.moving_left = True
-                if key == K_SPACE and self.player.alive and not self.player.in_air:
-                    self.player.jump = True
+                    self.moving_left = True #old
+                    self.movement[0] = True #new
+                if key == K_SPACE:
+                    self.player.velocity.y = -3 #new
+                if key == K_q:
+                    pg.quit()
+                    sys.exit()
             # Keyup Press
             if event.type == KEYUP:
                 key = event.key
                 if key == K_d or key == K_RIGHT:
-                    self.moving_right = False
+                    self.moving_right = False #old
+                    self.movement[1] = False #new
                 if key == K_a or key == K_LEFT:
-                    self.moving_left = False
+                    self.moving_left = False #old
+                    self.movement[0] = False #new
 
-        self.settings.move_left = self.moving_left
-        self.settings.move_right = self.moving_right
-
+        self.settings.move_left = self.moving_left # remove
+        self.settings.move_right = self.moving_right # remove
+        
     def play(self):
         while True:
-            self.events_checker(),
+            self.events_checker()
             self.update_entities()
             self.draw_entities()
 
@@ -83,42 +96,36 @@ class Game:
 
     def update_entities(self):
         # when player is alive
-        if self.player.alive:
+        if self.player.isAlive:
             # Update the animation
             self.player.update_animation()
-            # update jump action for jump
-            if self.player.in_air:
-                self.player.update_action("jump")
-            # update player's action
-            elif self.moving_left or self.moving_right:
-                # if moving, update action for run
-                self.player.update_action("run")
-            else:
-                # if not, update action for idle
-                self.player.update_action("idle")
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
 
-            # Update all entities
-            for entity in self.entities.values():
-                entity.move()
+            # Update boss and other items here
+            # TODO
 
     # display the map to the screen
-    def display_map(self, screen, tmx_data, world_offset):
+    def display_map(self, tmx_data, world_offset):
         for layer in tmx_data:
             # x,y,surface
             for tile in layer.tiles():
-                x_pixel = tile[0] * 16 + self.settings.world_offset[0]
-                y_pixel = tile[1] * 16 + self.settings.world_offset[1]
+                x_pixel = tile[0] * 16 - world_offset[0]
+                y_pixel = tile[1] * 16 - world_offset[1]
                 # draw surface onto the screen
                 self.screen.blit(tile[2], (x_pixel, y_pixel))
 
     def draw_entities(self):
+        self.offset[0] += (self.player.rect().centerx - self.settings.screen_width / 2 - self.offset[0]) / 30
+        self.offset[1] += (self.player.rect().centery - self.settings.screen_height / 3 - self.offset[1]) / 30
+        render_offset = (int(self.offset[0]), int(self.offset[1]))
+        # draw bg color before each loop
+        self.screen.blit(self.map_bg, (0, 0))
+        # draw the map
+        self.display_map(self.tmx_data, render_offset)
+
         for entity in self.entities.values():
-            # draw bg color before each loop
-            entity.draw_BG()
-            # draw the map
-            self.display_map(self.screen, self.tmx_data, self.world_offset)
             # draw players
-            entity.draw()
+            entity.draw(render_offset)
 
 
 if __name__ == "__main__":
---
+++ scripts/entities.py
@@ -0,0 +1,126 @@
+import pygame as pg
+from scripts.timer import Timer
+import os
+
+class PhysicsEntity:
+    def __init__(self, game, type, pos, size):
+        self.game = game
+        self.screen = game.screen
+        self.settings = game.settings
+        self.type = type
+        self.size = size
+        self.pos = list(pos) # Figure out why other than just making a new variable
+
+        self.velocity = pg.Vector2(0, 0)
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+        self.isJumping = False
+        self.isAlive = True # Used later to determine whether game is over
+
+
+        # Animation list
+        self.animations = {
+            "idle": Timer(self.load_images("idle", self.size[1] // 16), "idle"),
+            "run": Timer(self.load_images("run", self.size[1] // 16), "run"),
+            "jump": Timer(self.load_images("jump", self.size[1] // 16), "jump"),
+        }
+
+        self.current_animation = self.animations["idle"]
+        self.flip = False
+
+    def load_images(self, animation_name, scale):
+        # Define base path for player imgs
+        Base_Player_Path = "assets/Adventurer/Indvidual Sprites/"
+        # Load all images for the player
+        temp_list = []
+        # Count number of files in the folder
+        num_of_frames = len(os.listdir(Base_Player_Path + f"{animation_name}"))
+        for i in range(num_of_frames):
+            # Load player img
+            img = pg.image.load(
+                Base_Player_Path
+                + f"{animation_name}/adventurer-{animation_name}-0{i}-1.3.png"
+            ).convert_alpha()
+            # Scale player img if needed
+            img = pg.transform.scale(
+                img, (int(img.get_width() * scale), int(img.get_height() * scale))
+            )
+            temp_list.append(img)
+        return temp_list
+    
+    def update_animation(self):
+        self.current_animation.next_frame()
+    
+    def set_action(self, action):
+        # check if the new action is different from the previous one
+        if action != self.current_animation.action:
+            self.current_animation = self.animations[action]
+            self.current_animation.reset()
+    
+    def rect(self):
+        return pg.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def update(self, tilemap, movement=(0, 0)):
+        self.colliions = {'up': False, 'down': False, 'right': False, 'left': False}
+        
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
+
+        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        if self.colliions['down'] or self.colliions['up']:
+            self.velocity[1] = 0
+
+        self.animations.update()
+
+        
+    def draw(self, offset=(0, 0)):
+        self.screen.blit(pg.transform.flip(self.current_animation.image(), self.flip, False), (self.pos[0] - offset[0], self.pos[1] - offset[1]) )
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0 # For jumping animation
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisions['down']:
+            self.air_time = 0
+        
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
